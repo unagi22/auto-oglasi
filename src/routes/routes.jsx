1d95ccbaf0108c569ccc25d42a158bb0feeddb3b
omar-adzagic <omar.adzagic@gmail.com>
2023-09-29 15:10:03 +0200

    Add Admin route.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Posts from "../pages/posts";
 import About from "../pages/about";
 import Profile from "../pages/profile";
+import Admin from "../pages/admin.jsx";
 
 const AppRoutes = () => {
   return (
@@ -12,6 +12,7 @@ const AppRoutes = () => {
       <Route path="/posts" element={<Posts />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/about" element={<About />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
   );
 };
