897851b13c7792649f1371d43f4fefbd04b0509c
omar-adzagic <omar.adzagic@gmail.com>
2023-10-02 20:43:58 +0200

    Fix styling and allow single image or multiple images based on the `multiple` prop.

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -2,12 +2,12 @@ import { useRef } from 'react';
 import Button from "@mui/material/Button";
 import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 
-function ImageUploader({ buttonText, handleOpen, images, setImages }) {
+function ImageUploader({ buttonText, handleOpen, images, setImages, multiple = false }) {
     const inputRef = useRef(null);
 
     const handleImageChange = (e) => {
         const files = Array.from(e.target.files);
-        const newImages = [...images, ...files];
+        const newImages = multiple ? [...images, ...files] : files;
         setImages(newImages);
     };
 
@@ -25,17 +25,17 @@ function ImageUploader({ buttonText, handleOpen, images, setImages }) {
     return (
         <div>
             <Button variant="contained" onClick={openFilePicker}>{buttonText || 'Open'}</Button>
-            {/* Input to upload multiple images */}
-            <input ref={inputRef} hidden type="file" multiple onChange={handleImageChange} />
+            {/* Input to upload single or multiple images */}
+            <input ref={inputRef} style={{ display: 'none' }} type="file" accept="image/*" multiple={multiple} onChange={handleImageChange} />
 
             {/* Display the selected images */}
-            <div style={{ display: 'flex' }}>
+            <div style={{ display: 'flex', marginTop: '1rem' }}>
                 {images.map((image, index) => (
                     <div key={index} style={{position: 'relative'}}>
                         <img
                             src={URL.createObjectURL(image)}
                             alt="User-selected"
-                            style={{ width: '100px', margin: '10px' }}
+                            style={{ width: '100px', margin: '8px' }}
                         />
                         <HighlightOffOutlinedIcon
                             style={{ position: 'absolute', right: '0', top: '0', color: 'red', cursor: 'pointer', 'background': '#fff', borderRadius: '50%' }}
