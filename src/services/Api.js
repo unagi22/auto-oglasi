b919b3147d32c5643fc0a405b2e47b5a2be2b08a
omar-adzagic <omar.adzagic@gmail.com>
2023-10-04 00:03:37 +0200

    Refactor `Api` service and automatically refresh the `access` token.

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 class Api {
   constructor(baseUrl = "http://localhost:8000/api") {
     this.baseUrl = baseUrl;
-    this.refreshToken = null;
+    this.refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY5NjUzODk0MywiaWF0IjoxNjk2MzY2MTQzLCJqdGkiOiI1MjNlZGM5ZTNmNTg0OWJlOGQ5YTcyY2FhYWQ2ZGM5MCIsInVzZXJfaWQiOjE5LCJpc19zdXBlcnVzZXIiOnRydWV9.gWokPz_C9OL-dGEZFkrW8bCDDfMW2neCVhsXl-llf4A';
     this.isSuperuser = false;
   }
 
@@ -12,18 +12,12 @@ class Api {
       Api.instance = new Api();
     }
 
-    Api.instance.removeToken()
-    // if (!Api.instance.getCurrentToken()) {
-    const accessTokenValue = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk2MzUyODY2LCJpYXQiOjE2OTYyNjY0NjYsImp0aSI6IjhmZWU1ZTI2NGNlNTQzZjg5ZDlkYjk4MjBmYTlkODRiIiwidXNlcl9pZCI6MTksImlzX3N1cGVydXNlciI6ZmFsc2V9.qFewxzrlOoGcgcSl2Hy6vFWZDRI9o24JWL6YHlac_hY';
-    Api.instance.storeToken(accessTokenValue);
-    // }
-
-    if (!accessTokenValue) {
-      return null;
-    }
-    const tokenData = JSON.parse(atob(accessTokenValue.split('.')[1]))
-    if (tokenData.is_superuser !== Api.instance.isSuperuser) {
-      Api.instance.isSuperuser = tokenData.is_superuser;
+    let accessToken = Api.instance.getCurrentToken();
+    if (accessToken) {
+      const tokenData = JSON.parse(atob(accessToken.split('.')[1]))
+      if (tokenData.is_superuser !== Api.instance.isSuperuser) {
+        Api.instance.isSuperuser = tokenData.is_superuser;
+      }
     }
 
     return Api.instance;
@@ -45,7 +39,24 @@ class Api {
     return window.localStorage.getItem('accessToken');
   }
 
-  async refreshAccessToken() {}
+  async refreshAccessToken() {
+    const response = await fetch(this.getUrl('/token/refresh/'), {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        refresh: this.refreshToken
+      }),
+    });
+
+    const data = await response.json();
+    if (data.access) {
+      this.storeToken(data.access);
+      return data.access;
+    }
+    return null;
+  }
 
   async obtainTokens() {}
 
@@ -59,37 +70,65 @@ class Api {
     return {};
   }
 
-  get(path) {
-    return fetch(this.getUrl(path), {
-      method: "GET",
+  async fetchWithTokenRefresh(method, url, options = {}) {
+    let response = await fetch(this.getUrl(url), {
+      method: method,
       headers: this.getHeaders(),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+      ...options
+    });
+
+    if (response.status === 401) {
+      console.log('Access token expired');
+      try {
+        const newToken = await this.refreshAccessToken();
+        if (!newToken) {
+          throw new Error("Unable to refresh access token.");
         }
-        return response.json();
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
+
+        response = await fetch(this.getUrl(url), {
+          method: method,
+          headers: this.getHeaders(),
+          ...options
+        });
+      } catch (error) {
+        console.error("Error during token refresh:", error.message);
+        // Remove invalid tokens
+        this.removeToken();
+        // Assuming you're using something like react-router or a similar routing library:
+        window.location.href = '/login';
+
+        throw new Error("Session expired. Redirecting to login.");
+      }
+    }
+
+    return response;
+  }
+
+  async get(path) {
+    const response = await this.fetchWithTokenRefresh("GET", path);
+    if (!response.ok) {
+      throw new Error("Network response was not ok1");
+    }
+
+    return response.json();
   }
 
   post(url, payload = {}, headers = {}) {
-    const defaultHeaders = this.getHeaders();
-
-    return fetch(this.getUrl(url), {
-      method: "POST",
-      headers: new Headers({...defaultHeaders, ...headers}),
+    return this.fetchWithTokenRefresh("POST", url, {
+      headers: new Headers({ ...this.getHeaders(), ...headers }),
       body: payload,
-    })
+    });
+  }
+
+  patch(url, payload = {}, headers = {}) {
+    return this.fetchWithTokenRefresh("PATCH", url, {
+      headers: new Headers({ ...this.getHeaders(), ...headers }),
+      body: payload,
+    });
   }
 
   delete(url) {
-    return fetch(this.getUrl(url), {
-      method: "DELETE",
-      headers: new Headers(this.getHeaders()),
-    })
+    return this.fetchWithTokenRefresh("DELETE", url);
   }
 }
 
