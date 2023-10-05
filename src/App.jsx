00bcb466a643f02fe5e66ee1872a144f19f0d24b
omar-adzagic <omar.adzagic@gmail.com>
2023-10-05 21:55:55 +0200

    Conditionally show navigation buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Footer from "./components/footer";
 import AppRoutes from "./routes/routes";
 import "./App.css";
 import Api from "./services/Api.js";
+import {Button} from "@mui/material";
+
 
 const api = Api.getInstance();
 
 const App = () => {
-  return (
+    return (
     <Router>
       <div
         style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
@@ -19,19 +21,34 @@ const App = () => {
             <li>
               <Link to="/posts">Posts</Link>
             </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/admin">{api.isSuperuser ? 'Admin panel' : 'User data'}</Link>
-            </li>
+            {api.isAuthenticated ? (
+                <>
+                  <li>
+                    <Link to="/profile">Profile</Link>
+                  </li>
+                  <li>
+                    <Link to="/admin">{api.isSuperuser ? 'Admin panel' : 'User data'}</Link>
+                  </li>
+                  <li>
+                    <Button variant="contained" color="warning" onClick={() => api.logout()}>
+                      Logout
+                    </Button>
+                  </li>
+                </>
+            ) : (
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+            )}
           </ul>
         </nav>
-        <AppRoutes />
+        <div style={{ height: 'calc(100vh - 110px - 56px)', margin: 0 }}>
+          <AppRoutes />
+        </div>
         <Footer />
       </div>
-    </Router>
-  );
+  </Router>
+  )
 };
 
 export default App;
