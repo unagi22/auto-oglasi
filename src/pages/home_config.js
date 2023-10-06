955662ad4507f7dd44bc356d1e7f97f40a460596
omar-adzagic <omar.adzagic@gmail.com>
2023-10-06 22:36:47 +0200

    Add `orderFields` config fields for ordering.

---
+++ src/pages/home_config.js
@@ -0,0 +1,27 @@
+export const orderFields = [
+    {
+        id: 1,
+        name: 'Price',
+        value: 'price',
+    },
+    {
+        id: 2,
+        name: 'Manufacture year',
+        value: 'car__manufacture_year',
+    },
+    {
+        id: 3,
+        name: 'Mileage',
+        value: 'car__mileage',
+    },
+    {
+        id: 4,
+        name: 'Power (kw)',
+        value: 'car__power',
+    },
+    {
+        id: 5,
+        name: 'Registration date',
+        value: 'car__registration_date',
+    }
+];
