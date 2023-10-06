788da397710a1fe934446cc684ffa5cc94413433
omar-adzagic <omar.adzagic@gmail.com>
2023-10-06 22:17:53 +0200

    Fix column width for the email column.

--- src/pages/admin/users/index/config.js
+++ src/pages/admin/users/index/config.js
@@ -2,7 +2,7 @@ export const dataGridColumnsConfig = [
     { field: 'id', headerName: 'ID', width: 90 },
     { field: 'first_name', headerName: 'First name', width: 120 },
     { field: 'last_name', headerName: 'Last name', width: 120 },
-    { field: 'email', headerName: 'Email', width: 170 },
+    { field: 'email', headerName: 'Email', width: 210 },
     {
         field: 'country',
         headerName: 'Country',
