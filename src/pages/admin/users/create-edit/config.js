3fbc1b552c1d9147cfb2927015734dca61f26a32
omar-adzagic <omar.adzagic@gmail.com>
2023-10-06 19:02:06 +0200

    Add config for the create users form.

---
+++ src/pages/admin/users/create-edit/config.js
@@ -0,0 +1,16 @@
+const tab1 = ['first_name', 'last_name', 'email', 'email_verified'];
+const tab2 = ['country'];
+const tab3 = ['profile_picture'];
+
+const createGroupedObject = (keys, value) => {
+    return keys.reduce((acc, key) => {
+        acc[key] = value;
+        return acc;
+    }, {});
+};
+
+export const tabColumnsObj = {
+    ...createGroupedObject(tab1, 0),
+    ...createGroupedObject(tab2, 1),
+    ...createGroupedObject(tab3, 2),
+};
