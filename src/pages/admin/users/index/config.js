6fe6ff7fb93f75ade9dbbd46083967147249baa5
omar-adzagic <omar.adzagic@gmail.com>
2023-10-06 19:02:21 +0200

    Add config for users index page.

---
+++ src/pages/admin/users/index/config.js
@@ -0,0 +1,13 @@
+export const dataGridColumnsConfig = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'first_name', headerName: 'First name', width: 120 },
+    { field: 'last_name', headerName: 'Last name', width: 120 },
+    { field: 'email', headerName: 'Email', width: 170 },
+    {
+        field: 'country',
+        headerName: 'Country',
+        valueGetter: (params) => params.row.country.name,
+        width: 140,
+    },
+    { field: 'email_verified', headerName: 'Email verified', type: 'boolean', width: 130 },
+];
